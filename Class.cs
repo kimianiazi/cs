2add9ec59dc9170a6ab5b9b8066c119204a87ea4
kimianiazi <117292593+kimianiazi@users.noreply.github.com>
2023-01-18 22:54:18 +0330

    Add files via upload

---
+++ Class.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace حقوق_خالص
+{
+    class Class
+    {
+        public string name;
+        public string last;
+        public string number;
+        public int payment;
+        int maliat;
+
+        public int Income(int a)
+        {
+            
+            int bime = a * 7 / 100;
+            if (a > 6500)
+                maliat = a / 10;
+            else
+                maliat = 0;
+
+            return a - (bime + maliat);
+            
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace حقوق_خالص
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class employe = new Class();
+
+            Console.WriteLine("name?");
+            employe.name = Console.ReadLine();
+
+            Console.WriteLine("last?");
+            employe.last = Console.ReadLine();
+
+            Console.WriteLine("number?");
+            employe.number = Console.ReadLine();
+
+            Console.WriteLine("payment?");
+            employe.payment = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine(employe.Income(employe.payment));
+
+            Console.ReadKey();
+
+        }
+    }
+}
